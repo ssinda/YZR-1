1af0f15087b40a379708c26d4a4b800e5cfdd5a4
ds <ds0704@naver.com>
2016-07-29 13:55:11 +0900

    16.7.29 1시 54분

--- src/main/java/net/nigne/yzrproject/service/ReservationService.java
+++ src/main/java/net/nigne/yzrproject/service/ReservationService.java
@@ -1,9 +1,12 @@
 package net.nigne.yzrproject.service;
 
 import java.util.List;
+import java.util.Map;
 
 import net.nigne.yzrproject.domain.ReservationVO;
 
 public interface ReservationService {
 	public List<ReservationVO> getReservation_list(String member_id);
+	public Map<String,Object> getReservation(String member_id);
+	public long getReservationTotal(String member_id);
 }
